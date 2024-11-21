73b198165231258f505546e89cfd04f517f219f5
pranjalkumarjha <pranjalkumarjsspk@gmail.com>
2024-11-21 19:22:44 +0530

    added to feature:Drag Drop, ouline rectangle

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -74,7 +74,34 @@ const Canvas = () => {
         }
 
       }
+      if(selectedCurve.current!==-1){
+        const curve = curves.current[selectedCurve.current];
+        let outlineRectanglePosition = {
+          smallX:100000,
+          smallY:100000,
+          largeX:0,
+          largeY:0
+        }; 
+        for(let i=0;i<curve.length;i++){
+            outlineRectanglePosition.smallX = Math.min(outlineRectanglePosition.smallX,curve[i].x);
+            outlineRectanglePosition.smallY = Math.min(outlineRectanglePosition.smallY,curve[i].y);
+            outlineRectanglePosition.largeX = Math.max(outlineRectanglePosition.largeX,curve[i].x);
+            outlineRectanglePosition.largeY = Math.max(outlineRectanglePosition.largeY,curve[i].y);
 
+          }
+          // draw outline of selected curve
+          ctx.current.save();
+          console.log(outlineRectanglePosition);
+          ctx.current.strokeStyle = "blue";
+          ctx.current.lineWidth = 1;
+          ctx.current.strokeRect(
+            outlineRectanglePosition.smallX,
+            outlineRectanglePosition.smallY, 
+            Math.abs(outlineRectanglePosition.smallX-outlineRectanglePosition.largeX),
+            Math.abs(outlineRectanglePosition.smallY-outlineRectanglePosition.largeY)
+          );
+          ctx.current.restore();
+      }
       console.log('curve index selected: ', selectedCurve.current);
     }
     if (startingX.current === -1 && startingY.current === -1) { // starting(x,y) of an arc drawing
